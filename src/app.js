533583e6452aecafb6870624f393fe2747d638cd
Khaled Yousef <K.Yousef-c@tamkeentech.sa>
2024-07-05 19:17:17 +0300

    prevent rate limit

--- src/app.js
+++ src/app.js
@@ -1,51 +1,50 @@
-import morgan from 'morgan';
-import express from 'express';
-import cors from 'cors';
-import rateLimit from 'express-rate-limit';
-import { catchAsync } from './utils/catchAsync.js';
+import morgan from "morgan";
+import express from "express";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
+import { catchAsync } from "./utils/catchAsync.js";
 // create Express App
 
 const app = express();
 
-
-// global Middleware 
+// global Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(morgan('dev'));
-app.use(rateLimit({
-    windowMs: 60*60*1000,
-    max:100,
-    message: 'Too many requests from this IP, please try again in an hour'
-}));
+app.use(morgan("dev"));
+// app.use(rateLimit({
+//     windowMs: 60*60*1000,
+//     max:100,
+//     message: 'Too many requests from this IP, please try again in an hour'
+// }));
 
+// router
 
-// router 
-
-import userRouter from './routes/userRoutes.js';
-import categoryRouter from './routes/categoryRoutes.js';
-import subCategoryRouter from './routes/subCategoryRoutes.js';
-import productRouter from './routes/productRoutes.js'
-import AppError from './utils/AppError.js';
-import globalErrorHandler from './middlewares/globalerrorHandler.js';
-
+import userRouter from "./routes/userRoutes.js";
+import categoryRouter from "./routes/categoryRoutes.js";
+import subCategoryRouter from "./routes/subCategoryRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+import AppError from "./utils/AppError.js";
+import globalErrorHandler from "./middlewares/globalerrorHandler.js";
 
 // App Routes
 
-
-app.use('/api/v1', userRouter)
-app.use('/api/v1/categories', categoryRouter);
-app.use('/api/v1/subCategories', subCategoryRouter);
-app.use('/api/v1/products', productRouter);
+app.use("/api/v1", userRouter);
+app.use("/api/v1/categories", categoryRouter);
+app.use("/api/v1/subCategories", subCategoryRouter);
+app.use("/api/v1/products", productRouter);
 
 //  not found route for non exist routes
 
-app.all('*', (req, res , next)=>{
+app.all("*", (req, res, next) => {
+  return next(
+    new AppError(
+      `Not Found Page , No route with your ${req.originalUrl} URL`,
+      404
+    )
+  );
+});
 
-    return next(new AppError(`Not Found Page , No route with your ${req.originalUrl} URL`, 404))
-})
-
-
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
 export default app;
