96c191670a03473424bc846187eb671039b67cb6
Mustafa Ramadan <Mustafa.Ramadan165@gmail.com>
2024-07-05 20:08:15 +0300

    set the authentication and authorization middleware on admin routes

--- src/routes/subCategoryRoutes.js
+++ src/routes/subCategoryRoutes.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { createSubCategory, deleteSubCategory, getAllSubCategories, getFilteredSubCategories, getSubCategory, updateSubCategory } from "../controllers/subCategoryControllers.js";
 import validation from '../middlewares/validation.js';
 import Schema from '../validation/index.js';
+import isAuth from '../middlewares/authentication.js';
+import Authorization from '../middlewares/Authorization.js';
 
 const router = Router();
 
-
+router.use(isAuth, Authorization('admin'));
 router.post('/', validation(Schema.subCategorySchema.createSubCategory),createSubCategory);
 router.get('/', getAllSubCategories)
 router.get('/filtered', getFilteredSubCategories);
