c04f0169bf3205c7c6e7f5b0719ddff94ebf8428
Mustafa Ramadan <Mustafa.Ramadan165@gmail.com>
2024-07-08 12:39:24 +0300

    create get All Carts , get Cart by user ID and delete cart

--- src/controllers/cartControllers.js
+++ src/controllers/cartControllers.js
@@ -1,37 +1,85 @@
 import AppError from "../utils/AppError.js";
 import { catchAsync } from "../utils/catchAsync.js";
-import Cart from '../Db/models/cart.model.js'
+import Cart from "../Db/models/cart.model.js";
 
-export const createCart = catchAsync(async  (req, res ,next)=>{
+export const createCart = catchAsync(async (req, res, next) => {
+  const { user } = req.body;
 
-    const { user } = req.body;
+  const newCart = await Cart.create({ user });
 
-    const newCart = await Cart.create({user});
+  if (!newCart) return next(new AppError(`Couldn't Create new Cart`, 400));
 
-    if(!newCart) return next(new AppError(`Couldn't Create new Cart`, 400));    
+  res.status(201).json({
+    status: "success",
+    data: newCart,
+  });
+});
 
-    res.status(201).json({
+export const getAllCarts = catchAsync(async (req, res, next) => {
+  const allCarts = await Cart.find().populate({
+    path: "user",
+    select: "-__v -password -createdAt -updatedAt -passwordChangedAt",
+  });
+
+
+  if (!allCarts) return next(new AppError(`No Carts Found`, 404));
+
+  res.status(200).json({
+    status: "success",
+    data: allCarts,
+  });
+});
+
+
+export const getCartByUserId = catchAsync(async (req, res, next) => {
+
+    const {id} = req.params;
+
+    const userCart = await Cart.findOne({user:id}).populate({
+        path: 'user',
+        select: "-__v -password -createdAt -updatedAt -passwordChangedAt",
+    });
+
+
+    if(!userCart) return next(new AppError(`No User with this ID`, 400));
+
+
+    res.status(200).json({
         status: 'success',
-        data: newCart
+        data: userCart
     })
 });
 
 
-export const getAllCarts = catchAsync(async (req, res ,next)=>{
+export const deleteCart = catchAsync(async (req, res ,next)=>{
 
+  const {id} = req.params;
 
-    const allCarts = await Cart.find().populate({
-        path: 'user',
-        select: '-__v -password -createdAt -updatedAt -passwordChangedAt'
-    })
+  const deletedCart = await Cart.findByIdAndDelete(id);
 
+  if(!deletedCart) return next(new AppError(`No Cart With this id`, 400));
 
-    if(!allCarts) return next(new AppError(`No Carts Found`, 404));
-
-    res.status(200).json({
-        status: 'success',
-        data: allCarts
-    })
+  res.status(204).json({
+    status: 'success',
+    message: 'Cart Deleted Successfully'
+  })
 })
 
 
+export const getCart = catchAsync(async (req, res, next) => {
+
+  const {id} = req.params;
+
+  const currentCart = await Cart.findById(id).populate({
+    path: 'user',
+    select: "-__v -password -createdAt -updatedAt -passwordChangedAt",
+});
+
+if(!currentCart) return next(new AppError(`No Cart With this id`, 400));
+
+
+res.status(200).json({
+  status: 'success',
+  data: currentCart
+})
+})
