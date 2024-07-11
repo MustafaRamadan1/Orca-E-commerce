a4ea605ed24ad75d7c13f585d1226956b81f7da9
Mustafa Ramadan <Mustafa.Ramadan165@gmail.com>
2024-07-11 11:58:47 +0300

    make checkout route to create cart items for the user and send the payment link to the front , create helper function for payment and create intenation to handle multiple payment methods

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,92 +1,116 @@
-import {Router} from 'express';
-import Cart from '../Db/models/cart.model.js'
-import AppError from '../utils/AppError.js';
-import CartItem from '../Db/models/cartItem.model.js';
-import { countCartTotalPrice } from '../utils/helperFunc.js';
-import { getPaymentKeyCreditCard} from '../payment/paymentHandler.js';
+import { Router } from "express";
+import { catchAsync } from "../utils/catchAsync.js";
+import Cart from "../Db/models/cart.model.js";
+import AppError from "../utils/AppError.js";
+import CartItem from "../Db/models/cartItem.model.js";
+import {
+  countCartTotalPrice,
+  formatItemsForPayment,
+  generatePaymentLink,
+} from "../utils/helperFunc.js";
+import { createPaymentLinkMultiMethods } from "../payment/paymentHandler.js";
 const router = Router();
 
-// router.post('/pay',async (req, res ,next)=>{
+
+router.post("/pay", async (req, res, next) => {
+  const { cartItems } = req.body;
+  // create cart items and then get the cart id and find by id
+  try {
+    const newCartItem = await CartItem.create(cartItems);
+
+    if (newCartItem.length === 0)
+      return next(new AppError(`Couldn't Create Cart Items`, 400));
+
+    const currentCart = await Cart.findById(newCartItem[0].cart).populate({
+      path: "items",
+      populate: "product",
+    });
+
+    const totalPrice = countCartTotalPrice(currentCart.items);
+
+    const updatedCart = await Cart.findByIdAndUpdate(
+      currentCart._id,
+      { totalPrice },
+      { new: true, runValidator: true }
+    )
+      .populate({
+        path: "items",
+        populate: "product",
+      })
+      .populate("user");
+    const items = updatedCart.items.map((item) => {
+      return {
+        product_id: item.product._id.toString(),
+        name: item.product.name,
+        amount_cents: item.product.price * 100,
+        quantity: item.quantity,
+      };
+    });
+
+    const response = await getPaymentKeyCreditCard(
+      updatedCart.user,
+      totalPrice,
+      items,
+      process.env.PAYMOB_CARD_INTEGRATION
+    );
+
+    res.status(200).json({
+      status: "success",
+      data: response,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.post(
+  "/checkout",
+  catchAsync(async (req, res, next) => {
+    const { cartItems } = req.body;
+
+    const newCartItems = await CartItem.create(cartItems);
+
+    if (!newCartItems)
+      return next(new AppError(`Couldn't Create Cart Items`, 400));
+
+    const cart = await Cart.findById(newCartItems[0].cart)
+      .populate({
+        path: "items",
+        populate: "product",
+      })
+      .populate("user");
+
+    const totalPrice = countCartTotalPrice(cart.items);
     
-//     const {cart, product, quantity} = req.body;
-//     // create cart items and then get the cart id and find by id
-//     try{
-            
+    const updatedCart = await Cart.findByIdAndUpdate(
+      cart._id,
+      { totalPrice },
+      { new: true, runValidators: true }
+    )
+      .populate({
+        path: "items",
+        populate: "product",
+      })
+      .populate("user");
 
-//         const newCartItem = await CartItem.create({product, cart,quantity});
+    const formattedItems = formatItemsForPayment(updatedCart.items);
 
-//         if(!newCartItem) return next(new AppError(`Couldn't Create new Cart Item`, 400));
+    const response = await createPaymentLinkMultiMethods(
+      updatedCart.totalPrice,
+      [
+        +process.env.PAYMOB_CARD_INTEGRATION,
+        +process.env.PAYMOB_WALLET_INTEGRATION,
+      ],
+      formattedItems,
+      cart.user
+    );
 
-//         const currentCart = await Cart.findById(newCartItem.cart).populate({
-//             path:'items',
-//             populate:'product'
-//         });
+    const url = generatePaymentLink(response.data.client_secret);
+    res.status(200).json({
+      status: "success",
+      url,
+    });
+  })
+);
 
-//         const totalPrice = countCartTotalPrice(currentCart.items);
-
-//         const updatedCart = await Cart.findByIdAndUpdate(currentCart._id, {totalPrice}, {new:true, runValidator:true})
-//         .populate({
-//             path:'items',
-//             populate:'product'
-//         })
-
-        
-
-//         res.status(200).json({
-//             status:'success',
-//             data:updatedCart
-//         })
-//     }
-//     catch(err){
-//         console.log(err)
-//     }
-// })
-
-
-router.post('/pay',async (req, res ,next)=>{
-    
-        const {cartItems}= req.body;
-        // create cart items and then get the cart id and find by id
-        try{
-                
-    
-            const newCartItem = await CartItem.create(cartItems);
-    
-            if(newCartItem.length === 0) return next(new AppError(`Couldn't Create Cart Items`, 400));
-    
-            const currentCart = await Cart.findById(newCartItem[0].cart).populate({
-                path:'items',
-                populate:'product'
-            });
-    
-            const totalPrice = countCartTotalPrice(currentCart.items);
-    
-            const updatedCart = await Cart
-            .findByIdAndUpdate(currentCart._id, {totalPrice}, {new:true, runValidator:true})
-            .populate({
-                path:'items',
-                populate:'product'
-            }).populate('user')
-            const items = updatedCart.items.map((item)=>{
-                return { 
-                    product_id: item.product._id.toString(),
-                    name: item.product.name,
-                    amount_cents: item.product.price * 100,
-                    quantity: item.quantity
-                }
-            });
-
-           const response = await  getPaymentKeyCreditCard(updatedCart.user,totalPrice,items,process.env.PAYMOB_CARD_INTEGRATION);
-            
-    
-            res.status(200).json({
-                status:'success',
-                data:response
-            })
-        }
-        catch(err){
-            console.log(err)
-        }
-    })
-    
 export default router;
